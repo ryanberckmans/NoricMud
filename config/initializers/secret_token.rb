2575a1657c3d45c1c5667f692b5f5ddd8ff97264
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-02-21 14:32:05 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mud::Application.config.secret_token = 'a64d6965ba78ae0aaffc5a8565ff7ff665869a0bece2f94e9d36f695787bf9100defabd1bfee128262802506a0acbcde188a758d9faba8135c44ba819e6c1400'
