2bc5ad7aa8ed40d6602535b1036cd0d05a3ee2fa
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-03-05 17:22:22 -0500

    command_handler_spec examples

--- lib/command_handler.rb
+++ lib/command_handler.rb
@@ -1,18 +1,56 @@
+require 'depq'
 
 class CommandHandler
-
   def initialize
+    @mob_handlers = {}
+  end
+
+  def add(mob)
+    raise "mob already exists" if @mob_handlers[mob]
+    @mob_handlers[mob] = []
+  end
+
+  def remove(mob)
+    verify_exists mob
+    @mob_handlers.delete mob
   end
 
   def push_cmd_scope( mob )
-    mob.cmd_handlers.push []
+    verify_exists mob
+    @mob_handlers[mob].push Depq.new
   end
 
   def pop_cmd_scope( mob )
-    mob.cmd_handlers.pop
+    verify_exists mob
+    verify_has_scope mob
+    @mob_handlers[mob].pop
   end
 
-  def scope_size( mob )
-    mob.cmd_handlers.size
+  def add_handler(mob, handler, priority)
+    verify_exists mob
+    verify_has_scope mob
+    @mob_handlers[mob].last.insert handler, priority
+  end
+
+  def remove_handler( mob, handler )
+    verify_exists mob
+    verify_has_scope mob
+    removed = false
+    @mob_handlers[mob].last.each_locator do |loc|
+      if loc.value == handler
+        @mob_handlers[mob].last.delete_locator loc
+        removed = true
+      end
+    end
+    raise "no handler was matched for removal" unless removed
+  end
+
+  private
+  def verify_exists( mob )
+    raise "expected mob to be present in commandhandler list" unless @mob_handlers[mob]
+  end
+  
+  def verify_has_scope( mob )
+    raise "expected mob to have a cmd_scope" if @mob_handlers[mob].empty?
   end
 end
--- spec/lib/command_handler_spec.rb
+++ spec/lib/command_handler_spec.rb
@@ -8,33 +8,112 @@ describe CommandHandler do
 
     subject { @handler }
 
-    context "with a mob" do
+    context "with an unadded mob" do
       before :each do
         @mob = double("Mob")
-        @mob_cmd_handlers = []
-        @mob.stub(:cmd_handlers) do @mob_cmd_handlers end
       end
 
-      context "and a cmd_scope for the mob" do
+      shared_examples_for "mob doesn't exist" do
+        it "raises error on remove" do
+          expect { @handler.remove @mob }.to raise_error
+        end
+
+        it "raises error on push_cmd_scope" do
+          expect { @handler.push_cmd_scope @mob }.to raise_error
+        end
+
+        it "raises error on pop_cmd_scope" do
+          expect { @handler.pop_cmd_scope @mob }.to raise_error
+        end
+
+        it "raises error on add_handler" do
+          expect { @handler.add_handler @mob, Object.new, 10 }.to raise_error
+        end
+
+        it "raises error on remove_handler" do
+          expect { @handler.remove_handler @mob, Object.new }.to raise_error
+        end
+
+        it "allows mob to be added" do
+          expect { @handler.add @mob }.to_not raise_error
+        end
+      end # shared examples mob doesn't exist
+
+      it_behaves_like "mob doesn't exist"
+      
+      context "after mob is added" do
         before :each do
-          @handler.push_cmd_scope( @mob )
+          @handler.add @mob
         end
 
-        it "registers the scope for the mob" do
-          @handler.scope_size( @mob ).should == 1
+        it "raises error on pop_cmd_scope, because no scopes have been added" do
+          expect { @handler.pop_cmd_scope @mob }.to raise_error
         end
 
-        context "when the cmd_scope is popped for the mob" do
+        it "allows mob to be removed" do
+          expect { @handler.remove @mob }.to_not raise_error
+        end
+
+        context "and mob is then removed" do
           before :each do
-            @handler.pop_cmd_scope( @mob )
+            @handler.remove @mob
           end
-
-          it "registers the scope was popped for the mob" do
-            @handler.scope_size( @mob ).should == 0
-          end
+          it_behaves_like "mob doesn't exist"
         end
+        
+        context "with a cmd_scope" do
+          before :each do
+            @handler.push_cmd_scope @mob
+          end
 
-      end # context new cmd_scope
-    end # context with a mob
+          it "allows a single handler to be added" do
+            pending
+          end
+
+          it "allows three handlers with differing priorities to be added" do
+            pending
+          end
+
+          it "returns an empty array of handlers with a call to ordered_handlers" do
+            pending
+          end
+
+          context "with three handlers with differing priorities added" do
+            it "removes a specific handler" do
+              pending
+            end
+
+            it "returns a complete and ordered array of handlers with a call to ordered_handlers" do
+              pending
+            end
+
+            context "with an additional push_cmd_scope" do
+              it "returns an empty array of handlesr with a call to ordered_handlers, because the newest cmd_scope is blank" do
+                pending
+              end
+              context "when the additional cmd_scope is popped" do
+                it "again behaves like when the three handlers with diff priorites were added" do
+                  pending
+                end
+              end
+            end
+          end
+
+          it "allows a pop_cmd_scope" do
+            expect { @handler.pop_cmd_scope( @mob ) }.to_not raise_error
+          end
+
+          context "when the cmd_scope is popped for the mob" do
+            before :each do
+              @handler.pop_cmd_scope @mob
+            end
+
+            it "raises error on pop_cmd_scope, because no scopes remain" do
+              expect { @handler.pop_cmd_scope( @mob ) }.to raise_error
+            end
+          end # context cmd_scope popped
+        end # context new cmd_scope
+      end # context when mob is added
+    end # context with an unadded mob
   end # context an instance
 end
