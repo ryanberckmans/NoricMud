339c6afb39001fd0a1f4c71ed7a34162644ceb78
Ryan Berckmans <ryan.berckmans@gmail.com>
2011-03-11 17:25:39 -0500

    bin/mud, refactored mud setup into lib/mud.rb, spec_helper uses lib/mud.rb; logfiles now named after RAILS_ENV instead of mud.log/spec.log

---
+++ lib/mud.rb
@@ -0,0 +1,67 @@
+
+require 'fiber'
+require 'bundler'
+Bundler.setup
+require "active_record"
+ActiveRecord::IdentityMap.enabled = true
+require "yaml"
+
+ActiveRecord::Base.establish_connection YAML.load_file('config/database.yml')[ ENV['RAILS_ENV'] ]
+
+require "models/account.rb"
+require "models/character.rb"
+require "models/mob.rb"
+require "models/exit.rb"
+require "models/room.rb"
+
+require "util.rb"
+require "log.rb"
+
+Log::get ENV['RAILS_ENV'], { :default => true, :level => Logger::DEBUG }
+Log::info "============================== booting ==============================", "mud"
+
+TICK_DURATION = 0.25 # in seconds
+
+Log::info RUBY_VERSION, "ruby version"
+Log::info "#{TICK_DURATION.to_s}s", "tick duration"
+
+require "pov.rb"
+require "abbrev_map.rb"
+require "network.rb"
+require "account_system.rb"
+require "character_system.rb"
+require "mob_commands.rb"
+require "game/base.rb"
+
+def start_mud
+  Log::info "instantiating core components", "mud"
+  
+  network = Network.new
+  auth = Authentication.new network
+  account_system = AccountSystem.new network, auth
+  character_selection = CharacterSelection.new account_system
+  character_system = CharacterSystem.new account_system, character_selection
+  game = Game.new character_system
+
+  Log::info "starting tick loop", "mud"
+  
+  begin
+    while true
+      tick_start = Time.now
+      Log::info "start tick", "mud"
+      network.tick
+      account_system.tick
+      character_system.tick
+      game.tick
+      tick_duration = Time.now - tick_start
+      time_remaining = [0,TICK_DURATION - tick_duration].max
+      Log::info "end tick, duration #{"%4.6f" % tick_duration}s, #{"%4.6f" % (tick_duration / TICK_DURATION * 100)}% capacity, sleeping #{"%4.6f" % time_remaining}s", "mud"
+      sleep time_remaining
+    end
+  rescue Exception => e
+    Log::fatal "uncaught exception #{e.class}", "mud"
+    Log::fatal e.backtrace.join "\t"
+    Log::fatal e.message if e.message.length > 0 
+  end
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,6 @@
-require 'fiber'
-require 'bundler'
-Bundler.setup
-require "active_record"
-require "yaml"
 
-ActiveRecord::Base.establish_connection YAML.load_file('config/database.yml')[ ENV['RAILS_ENV'] ]
+ENV['RAILS_ENV'] = "test"
 
-require "models/exit.rb"
-require "models/account.rb"
-require "models/character.rb"
-require "models/mob.rb"
-require "models/room.rb"
-
-require "util.rb"
-require "log.rb"
-
-Log::get "spec", { :default => true, :level => Logger::DEBUG }
-Log::info "============================== booting ==============================", "mud"
-
-TICK_DURATION = 0.25 # in seconds
-
-Log::info RUBY_VERSION, "ruby version"
-Log::info "#{TICK_DURATION.to_s}s", "tick duration"
-
-require "pov.rb"
-require "abbrev_map.rb"
-require "mob_commands.rb"
-require "network.rb"
-require "account_system.rb"
-require "character_system.rb"
-require "game/base.rb"
+require "mud.rb"
 
 $test_port = Random.new.rand 10000..50000
