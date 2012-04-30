4d6d25205b4b74edb4a9047a1ade21f3fbe6fd25
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-04-29 23:39:05 -0400

    implement MudObject#save and #persist? and spec

--- lib/noric_mud/mud_object.rb
+++ lib/noric_mud/mud_object.rb
@@ -1,4 +1,15 @@
 module NoricMud
   class MudObject
+    def initialize persistence=nil
+      @persistence = persistence
+    end
+
+    def save
+      @persistence.async_save self if @persistence
+    end
+
+    def persist?
+      !@persistence.nil?
+    end
   end
 end
--- spec/noric_mud/mud_object_spec.rb
+++ spec/noric_mud/mud_object_spec.rb
@@ -2,6 +2,30 @@ require "spec_helper"
 
 module NoricMud
   describe MudObject do
+    context "with persistence" do
+      before :each do
+        @persistence = double
+        @mud_object = MudObject.new @persistence
+      end
 
+      it "returns true for persist?" do
+        @mud_object.persist?.should be_true
+      end
+
+      it "calls async_save on save" do
+        @persistence.should_receive(:async_save).with(@mud_object).once
+        @mud_object.save
+      end
+    end
+
+    context "without persistence" do
+      before :each do
+        @mud_object = MudObject.new
+      end
+
+      it "returns false for persist?" do
+        @mud_object.persist?.should be_false
+      end
+    end
   end
 end
