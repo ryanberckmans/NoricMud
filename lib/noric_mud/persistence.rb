1a3c4656a7ca485819b8e7aebd107140f9021ba4
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-09 22:13:42 -0500

    persistence/object_persistence_sequel wip

--- lib/noric_mud/persistence.rb
+++ lib/noric_mud/persistence.rb
@@ -1,1 +1,60 @@
+require 'json'
+require 'bijection'
 require_relative "persistence/object_persistence_sequel"
+
+module NoricMud
+  module Persistence
+    public
+
+    def self.world_load object_id
+    end
+
+    def self.instance_load object_id
+    end
+
+
+    # R in CRUD
+    # Recursively loads from db the object subtree rooted at the passed object_id
+    def self.public_load object_id
+      
+      
+      #object_attributes = AttributeModel.find :object_id => object_id
+
+      #object = nil# construct proper object using object_attributes
+
+      #contained_object_models = ObjectModel.find :location_object_id => object_id # Object table has index on location_object_id
+
+      #contained_object_models.each do |contained_object_model|
+      #  object.contents << load contained_object_model.id
+      #end
+
+      #object
+    end
+
+    private
+      
+    @@class_map = Bijection.new
+    @@class_map.add NoricMud::Object, :object
+
+    def self.class_to_value klass
+      @class_map.get_y klass
+    end
+
+    def self.value_to_class value
+      @class_map.get_x value
+    end
+
+    # Serialize the passed attribute value. Values are wrapped in { :data => value } so literal values don't generate json parse errors.
+    #  E.g. "foo" isn't valid json, but "{ :data => "foo" }" is valid json.
+    def self.serialize_attribute_value value
+      { :data => value }.to_json
+      # todo catch serialization errors
+    end
+
+    # Deserialize the passed attribute value. Note the { :data => value } wrapper explained above.
+    def self.deserialize_attribute_value serialized_value
+      (JSON.parse serialized_value)[:data]
+      # todo catch parse errors
+    end 
+  end
+end
--- lib/noric_mud/persistence/object_persistence_sequel.rb
+++ lib/noric_mud/persistence/object_persistence_sequel.rb
@@ -5,56 +5,68 @@ Sequel.extension :migration
 
 module NoricMud
   module Persistence
-    def self.migrate sequel_db
-      Sequel::Migrator.run sequel_db, 'db/migrations', :use_transactions => true
-      sequel_db
-    end
+    class Sequel
+      class << self
+        public
+        def world_load_attributes object_id
+          load_attributes object_id, @@world_db
+        end
 
-    def self.connect_db yml_path
-      config = YAML.load_file(yml_path)[ ENV['RAILS_ENV'] ]
-      Sequel.connect( 
-                     config['database'],
-                     :max_connections => config['pool'],
-                     :pool_timeout => config['timeout'],
-                     :test => true
-                     )
-    end
-    
-    @@world_db    = migrate connect_db 'config/world_database.yml'
-    @@instance_db = migrate connect_db 'config/instance_database.yml'
+        def instance_load_attributes object_id
+          load_attributes object_id, @@instance_db
+        end
 
-    # R in CRUD
-    # Recursively loads from db the object subtree rooted at the passed object_id
-    def self.load object_id
-      #object_attributes = AttributeModel.find :object_id => object_id
+        private
+        def self.migrate sequel_db
+          Sequel::Migrator.run sequel_db, 'db/migrations', :use_transactions => true
+          sequel_db
+        end
 
-      #object = nil# construct proper object using object_attributes
+        private
+        def self.connect_db yml_path
+          config = YAML.load_file(yml_path)[ ENV['RAILS_ENV'] ]
+          Sequel.connect( 
+                         config['database'],
+                         :max_connections => config['pool'],
+                         :pool_timeout => config['timeout'],
+                         :test => true
+                         )
+        end
+        
+        @@world_db    = migrate connect_db 'config/world_database.yml'
+        @@instance_db = migrate connect_db 'config/instance_database.yml'
 
-      #contained_object_models = ObjectModel.find :location_object_id => object_id # Object table has index on location_object_id
+        # Return all attributes in a hash { :attribute_name => "<serialized json>" } for the passed object id.  Note that the attribute values are serialized JSON at this point.
+        def self.load_attributes object_id, sequel_database
+          object_attributes_dataset = sequel_database[:attributes].where(:object_id => object_id).select(:name, :value).all
 
-      #contained_object_models.each do |contained_object_model|
-      #  object.contents << load contained_object_model.id
-      #end
+          object_attributes = {}
+          object_attributes_dataset.each do |object_attribute|
+            object_attributes[object_attribute[:name].to_sym] = object_attribute[:value]
+          end
 
-      #object
-    end
+          object_attributes
+        end
 
-    # Each persisted Object has exactly one ObjectPersistence
-    class ObjectPersistence
+        # Each persisted Object has exactly one ObjectPersistence
+        class TODO_CHANGE_ME_ObjectPersistence
 
-      # C in CRUD
-      # Construct an ObjectPersistence used to persist a single Object
-      # Any children of the Object are persisted separately
-      def initialize object_to_persist
+          # C in CRUD
+          # Construct an ObjectPersistence used to persist a single Object
+          # Any children of the Object are persisted separately
+          def initialize object_to_persist
+          end
+
+          public
+          def location_object_id=
+          end
+          
+          def save_attribute attribute_name, attribute_value
+          end
+
+        end
+        
       end
-
-      public
-      def location_object_id=
-      end
-      
-      def save_attribute attribute_name, attribute_value
-      end
-
     end
   end
 end
