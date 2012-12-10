7001e7b30d3316139cd51ff7df6f911c9a3afce6
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-09 21:54:08 -0500

    object.rb: comment out wip to make it compile

--- lib/noric_mud/object.rb
+++ lib/noric_mud/object.rb
@@ -1,45 +1,45 @@
 module NoricMud
   class Object
     # db columns on all mud objects:
-    parent_mud_object_id # i.e. location.persistence_id
+    #parent_mud_object_id # i.e. location.persistence_id
     
     # db attributes on all mud objects:
-    short_name
-    long_name
-    description
-    keywords
+    #short_name
+    #long_name
+    #description
+    #keywords
 
     # transient attributes on all mud objects:
-    contents
+    #contents
 
     # instance variables
-    attributes
+    #attributes
     
     # public operations
-    bool persist?
-    void persist
-    id persistence_id
-    location=
-    location
-    short_name=
-    short_name
-    long_name=
-    long_name
-    description=
-    description
-    [string] keywords
-    add_keyword
-    remove_keyword
+    #bool persist?
+    #void persist
+    #id persistence_id
+    #location=
+    #location
+    #short_name=
+    #short_name
+    #long_name=
+    #long_name
+    #description=
+    #description
+    #[string] keywords
+    #add_keyword
+    #remove_keyword
 
     # private operations
-    void set_attribute attribute_name, attribute_value
-    void persist_attribute attribute_name, attribute_value
-    void create_new_persistence
-    bool persistence_exists?
+    #void set_attribute attribute_name, attribute_value
+    #void persist_attribute attribute_name, attribute_value
+    #void create_new_persistence
+    #bool persistence_exists?
 
     # overridden operations
-    bool persisted_attribute? attribute_name
-    klass perssitence_class
+    #bool persisted_attribute? attribute_name
+    #klass perssitence_class
     
     def initialize persistence=nil
       @persistence = persistence
