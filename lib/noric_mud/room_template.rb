1b3e442624c9064a9af77dda78893db570624f18
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-13 00:43:50 -0500

    add room_instance_id methods to RoomTemplate and spec

--- lib/noric_mud/room_template.rb
+++ lib/noric_mud/room_template.rb
@@ -8,6 +8,42 @@ module NoricMud
     include ObjectParts::ShortName
     include ObjectParts::Description
 
+    def initialize params={}
+      super params
+      set_attribute_unless_exists :room_instance_ids, []
+    end
+
+    # Add a new room_instance_id to this RoomTemplate, denoting a usage of this room in a containing ZoneTemplate
+    # @param room_instance_id - the room_instance_id to add
+    # @return nil
+    def add_room_instance_id room_instance_id
+      raise "#{room_instance_id} is already a room_instance_id and must be unique" if get_attribute(:room_instance_ids).include? room_instance_id
+      get_attribute(:room_instance_ids) << room_instance_id
+      nil
+    end
+
+    # Delete a room_instance_id from this RoomTemplate
+    # @param room_instance_id - the room_instance_id to delete
+    # @return nil
+    def delete_room_instance_id room_instance_id
+      get_attribute(:room_instance_ids).delete room_instance_id
+      nil
+    end
+
+    # Clear all room_instance_ids from this RoomTemplate
+    # @return nil
+    def clear_room_instance_ids
+      get_attribute(:room_instance_ids).clear
+      nil
+    end
+
+    # Return an Enumerator of all room_instance_ids for this RoomTemplate
+    # Each room_instance_id corresponds to a usage of this RoomTemplate in a containing ZoneTemplate
+    # @return Enumerator |room_instance_id|
+    def each_room_instance_id
+      get_attribute(:room_instance_ids).each
+    end
+    
     # Instantiate a Room object based on this RoomTemplate
     # @return Room the newly-constructed Room based on this RoomTemplate
     def roll
--- spec/noric_mud/room_template_spec.rb
+++ spec/noric_mud/room_template_spec.rb
@@ -6,6 +6,29 @@ module NoricMud
     its("class.ancestors") { should include(ObjectParts::ShortName) }
     its("class.ancestors") { should include(ObjectParts::Description) }
 
+    it { expect {subject.add_room_instance_id 0}.to change {subject.each_room_instance_id.to_a.size}.from(0).to(1) }
+    it { subject.add_room_instance_id(777).should be_nil }
+    
+    context "added a room_instance_id" do
+      before :each do
+        subject.add_room_instance_id 55
+      end
+      its(:each_room_instance_id) { should include(55) }
+      it { expect {subject.add_room_instance_id 55}.to raise_error }
+      it { expect {subject.delete_room_instance_id 55}.to change {subject.each_room_instance_id.to_a.size}.from(1).to(0) }
+      it { expect {subject.delete_room_instance_id 55}.to change {subject.each_room_instance_id.include? 55}.from(true).to(false) }
+      it { subject.delete_room_instance_id(55).should be_nil }
+      it { expect {subject.clear_room_instance_ids}.to change {subject.each_room_instance_id.to_a.size}.from(1).to(0) }
+      it { subject.clear_room_instance_ids.should be_nil }
+    end
+
+    context "initialized with params" do
+      subject { RoomTemplate.new({ :persistence_id => 5, :location => 7}) }
+      its(:persistence_id) { should eq(5) }
+      its(:location) { should eq(7) }
+      its("each_room_instance_id.to_a.size") { should eq(0) }
+    end
+
     context "with some basic properties" do
       before :each do
         subject.short_name = "some short name"
