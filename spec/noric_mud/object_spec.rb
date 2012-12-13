dadd43c96107beb39d6a76b813a83a688d854fef
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-13 04:57:58 -0500

    add a pending note to object_spec

--- spec/noric_mud/object_spec.rb
+++ spec/noric_mud/object_spec.rb
@@ -5,7 +5,7 @@ module NoricMud
   describe Object do
     pending "inject database"
     pending "#delete_attribute"
-    pending "modifying an attribute without calling set_attribute, such as modifying an array, won't save it"
+    pending "modifying an attribute without calling set_attribute, such as modifying an array, won't save it. Maybe this is a don't-play-with-fire thing. #modify_attribute can yield the value and persist it afterwards, and the burden is on the programmer not to modify attributes separately. That's a bit ghetto, isn't Evennia's system entirely transparent?  You set an attribute and it's magically updated, regardles of the complexity/depth of the attribute."
     
     it "set_attribute_unless_exists sets the attribute" do
       bar = [1,2,3]
