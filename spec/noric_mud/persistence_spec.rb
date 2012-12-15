ed92dc950dff8fe60701738d4395180b80c5b7d8
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-14 21:35:41 -0500

    persistence_spec wip

--- spec/noric_mud/persistence_spec.rb
+++ spec/noric_mud/persistence_spec.rb
@@ -3,7 +3,18 @@ require 'noric_mud/persistence'
 
 module NoricMud
   describe Persistence do
-    pending "#delete_attribute"
     pending "get_object shouldn't have a location param, and delegate to a private get_object which does allow a location parameter for recursive construction. Top-level get_objects should have set_location nil called and returned as detached, to prevent a de-sync between the object's db location and in-game location"
+
+    it "set_location passes params to Storage::set_location" do
+      params = {}
+      subject::Storage.should_receive(:set_location).once.with params
+      subject::set_location params
+    end
+
+    it "set_location returns the result of Storage::set_location" do
+      result = Object.new
+      subject::Storage.should_receive(:set_location).and_return(result)
+      subject::set_location(nil).should eq(result)
+    end
   end
 end
