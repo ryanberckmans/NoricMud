bc17170804ca4d65a536ca6977b4432df0db0c05
Ryan Berckmans <ryan.berckmans@gmail.com>
2012-12-30 18:46:59 -0500

    object spec wip

--- spec/noric_mud/object_spec.rb
+++ spec/noric_mud/object_spec.rb
@@ -3,6 +3,8 @@ require 'noric_mud/object'
 
 module NoricMud
   describe Object do
+    pending "location= should raise if location is set to self. What about location cycles?"
+
     pending "modifying an attribute without calling set_attribute, such as modifying an array, won't save it. Maybe this is a don't-play-with-fire thing. #modify_attribute can yield the value and persist it afterwards, and the burden is on the programmer not to modify attributes separately. That's a bit ghetto, isn't Evennia's system entirely transparent?  You set an attribute and it's magically updated, regardles of the complexity/depth of the attribute."
 
     pending "the entire persistence api should be protected - i.e. accessible by other Object instances - except persist() which is really the only client-facing operation"
