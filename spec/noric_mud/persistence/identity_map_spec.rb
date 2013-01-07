a8a3788d2f0173bc5798a1e05391e84b2f50d23f
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-01-07 11:24:44 -0500

    fix platform-independence bug in identity_map_spec

--- spec/noric_mud/persistence/identity_map_spec.rb
+++ spec/noric_mud/persistence/identity_map_spec.rb
@@ -5,7 +5,7 @@ module NoricMud
   module Persistence
     describe IdentityMap do
       it { subject[:key_not_found].should be_nil }
-      it { expect { subject[:get] = :works }.to change{subject[:get]}.from(nil).to(:works) }
+      it { expect { subject[:get] = "works" }.to change{subject[:get]}.from(nil).to("works") }
       
       context "with a value set that has a strong reference" do
         before :each do
@@ -14,7 +14,7 @@ module NoricMud
           subject[@key] = @value
           10.times { ObjectSpace.garbage_collect }
         end
-        it { subject[@key].should eq(@value) } unless JRUBY_VERSION # cannot force garbage collection in jruby
+        it { subject[@key].should eq(@value) } unless defined? JRUBY_VERSION # cannot force garbage collection in jruby
       end
 
       context "with a value set that has no strong reference" do
@@ -23,7 +23,7 @@ module NoricMud
           subject[@key] = Object.new
           10.times { ObjectSpace.garbage_collect }
         end
-        it { subject[@key].should be_nil } unless JRUBY_VERSION # cannot force garbage collection in jruby
+        it { subject[@key].should be_nil } unless defined? JRUBY_VERSION # cannot force garbage collection in jruby
       end
     end
   end
