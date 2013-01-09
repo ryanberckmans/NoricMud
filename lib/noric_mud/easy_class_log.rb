4385da1c4ff9b2b01db4f77db960a4f148812fd0
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-01-09 15:48:40 -0500

    EasyClassLog now supports use from class methods

--- lib/noric_mud/easy_class_log.rb
+++ lib/noric_mud/easy_class_log.rb
@@ -3,24 +3,34 @@ require_relative 'log'
 module NoricMud
   # EasyClassLog annotates log messages with the class name
   module EasyClassLog
+    if self.is_a? Module
+      def easy_class_log_name
+        name
+      end
+    else
+      def easy_class_log_name
+        self.class.name
+      end
+    end
+    
     def fatal &block
-      Log::fatal self.class.name, &block
+      Log::fatal easy_class_log_name, &block
     end
 
     def error &block
-      Log::error self.class.name, &block
+      Log::error easy_class_log_name, &block
     end
 
     def warn &block
-      Log::warn self.class.name, &block
+      Log::warn easy_class_log_name, &block
     end
 
     def info &block
-      Log::info self.class.name, &block
+      Log::info easy_class_log_name, &block
     end
 
     def debug &block
-      Log::debug self.class.name, &block
+      Log::debug easy_class_log_name, &block
     end
   end
 end
