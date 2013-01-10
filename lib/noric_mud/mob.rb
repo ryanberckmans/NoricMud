e9b2ee3dcca6b6361cb0f454aa2a2bbcacdb5577
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-01-09 23:37:12 -0500

    add Mob#short_appearance, #long_appearance; add 'player api' to Mob: #has_session?/=, #lost_link?=, #msg_mailbox, #run_cmd, #msg; add Room#long_appearance

--- lib/noric_mud/mob.rb
+++ lib/noric_mud/mob.rb
@@ -10,5 +10,55 @@ module NoricMud
     include ObjectParts::LongName
     include ObjectParts::Description
     include ObjectParts::Gender
+
+    def initialize params={}
+      super
+      @lost_link = false
+      @has_session = false
+    end
+    
+    # Short appearance is used when Mob is seen a room
+    def short_appearance
+      "{!{FY#{long_name} {FGis here.#{@lost_link ? " [Lost Link]" : ""}\n"
+    end
+
+    # Long appearance is used when Mob is looked at
+    def long_appearance
+      # How Med does it:
+      #  description
+      #  glance
+      #  eq
+      #  inventory
+      "{!{FG#{description}\n\n{FY{#{short_name} is in excellent condition."
+    end
+
+    # Run a cmd from the point of view of this Mob
+    def run_cmd cmd
+      msg { "Darn, you can't run commands yet" }
+    end
+
+    attr_accessor :msg_mailbox # a proc taking |msg| responsible for delivering the msg, set externally
+
+    def lost_link?
+      @lost_link
+    end
+
+    def lost_link= lost_link
+      @lost_link = lost_link
+    end
+
+    def has_session?
+      @has_session
+    end
+
+    def has_session= has_session
+      @has_session = has_session
+    end
+    
+    def msg &block
+      return unless @has_session
+      msg_mailbox.call yield if block_given?
+      nil
+    end
   end
 end
--- lib/noric_mud/room.rb
+++ lib/noric_mud/room.rb
@@ -6,5 +6,15 @@ module NoricMud
   class Room < Object
     include ObjectParts::ShortName
     include ObjectParts::Description
+
+    # Look appearance is used when looking in this room
+    def long_appearance
+      look = "{!{FY#{short_name}\n"
+      look += "{FM#{description}\n" unless not description or description.empty?
+      contents.each do |object_in_room|
+        look += object_in_room.short_appearance
+      end
+      look
+    end
   end
 end
